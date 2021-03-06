2bc20ca28697c1a0faf2014f2c0de06822927a2b
luohy15 <luohycs@gmail.com>
2021-03-06 23:56:35 +0800

    54-spiral-matrix

--- 2021/03/06/54-spiral-matrix/test_54.py
+++ 2021/03/06/54-spiral-matrix/test_54.py
@@ -4,4 +4,4 @@ from main_54 import Solution
 
 def test1():
     matrix = [[1,2,3],[4,5,6],[7,8,9]]
-    assert Solution().traverse(matrix) == [1,2,3,6,9,8,7,4,5]
+    assert Solution().spiralOrder(matrix) == [1,2,3,6,9,8,7,4,5]
